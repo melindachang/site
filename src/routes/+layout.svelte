c96b04eb34ff51a89e4fe23e3948e9874806bfa2
Melinda Chang <melindachang.hy@gmail.com>
2025-01-22 15:40:58 -0600

    restoring revamp pt 1

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Navigation from "$lib/components/Navigation.svelte";
+  import "$lib/sass/app.sass";
+
+  let { children } = $props();
+</script>
+
+<main>
+  <Navigation />
+  {@render children()}
+</main>
+
+<style lang="sass">
+  @use '../lib/sass/_variables'
+  
+  main
+    width: variables.$content-width
+</style>
